be11a5994d84603d3d6aade233120e55b46d8e21
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-12-23 17:22:00 +0100

    Multiple improvements
    - Use cache
    - Use rsync to get Qubes mirror files

--- snapshot.py
+++ snapshot.py
@@ -19,76 +19,87 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import os
 import json
 import logging
 import requests
 import hashlib
+import subprocess
 
 from dateutil.parser import parse as parsedate
-
-from flask import Flask, jsonify, Response
+from flask import Flask, Response
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 DEBIAN_SNAPSHOT = 'http://snapshot.debian.org'
 
-class ApiError(Exception):
-    status_code = 400
 
-    def __init__(self, message, status_code=None, payload=None):
-        super().__init__()
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv["message"] = self.message
-        return rv
-
-
-# begin flask app
-@app.errorhandler(ApiError)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
-
+# Useful function to get snapshot content type
 def get_file_info(url):
     info = {}
-    resp = requests.get(url)
-    info["status_code"] = resp.status_code
-    if resp.ok:
-        m = hashlib.md5()
-        for data in resp.iter_content(8192):
-            m.update(data)
-        info["hash"] = m.hexdigest()
-        info["first_seen"] = parsedate(
-            resp.headers["last-modified"]).strftime("%y%m%dT%H%M%SZ")
-        info["size"] = len(resp.content)
+    try:
+        resp = requests.get(url)
+        info["status_code"] = resp.status_code
+        if resp.ok:
+            m = hashlib.md5()
+            for data in resp.iter_content(8192):
+                m.update(data)
+            info["hash"] = m.hexdigest()
+            info["first_seen"] = parsedate(
+                resp.headers["last-modified"]).strftime("%Y%m%dT%H%M%SZ")
+            info["size"] = len(resp.content)
+    except requests.exceptions.ConnectionError:
+        pass
 
     return info
 
 
+# WIP: Get Qubes repo Debian content
+# This is to be modified to include Fedora, Archlinux etc.
+@cache.cached(timeout=300, key_prefix='all_files')
+def get_repo_files():
+    files = []
+    cmd = ["rsync", "--list-only", "--recursive",
+           "rsync://deb.qubes-os.org/qubes-mirror/repo/deb/"]
+    result = subprocess.check_output(cmd)
+    lines = result.decode('utf8').strip('\n').split('\n')
+    for line in lines:
+        line = line.split()
+        if not line[-1].startswith('r4.1'):
+            continue
+        if line[-1].endswith('.deb') or \
+                line[-1].endswith('.dsc') or \
+                line[-1].endswith('.deb') or \
+                line[-1].endswith('.tar.xz') or \
+                line[-1].endswith('.tar.bz2') or \
+                line[-1].endswith('.tar.gz'):
+            files.append(line[-1].strip())
+    return files
+
+
 @app.route("/mr/package/<string:srcpkgname>/<string:srcpkgver>/srcfiles",
            methods=["GET"])
+@cache.cached(timeout=3600)
 def get_src(srcpkgname, srcpkgver):
-    """
-    GET
-    """
-
-    result = {}
+    api_result = {}
     status_code = 404
 
-    debian_endpoint = '{base_url}/mr/package/{pkg_name}/{pkg_ver}/srcfiles?fileinfo=1'.format(
-        base_url=DEBIAN_SNAPSHOT, pkg_name=srcpkgname, pkg_ver=srcpkgver)
-    resp = requests.get(debian_endpoint)
+    debian_endpoint = \
+        '{base_url}/mr/package/{pkg_name}/{pkg_ver}/srcfiles?fileinfo=1'.format(
+            base_url=DEBIAN_SNAPSHOT, pkg_name=srcpkgname, pkg_ver=srcpkgver)
+    try:
+        resp = requests.get(debian_endpoint)
+    except requests.exceptions.ConnectionError:
+        return Response(api_result, status=status_code,
+                        mimetype="application/json")
+
     if resp.ok:
-        result = resp.content
+        api_result = resp.content
         status_code = resp.status_code
     else:
         if srcpkgname.startswith("lib"):
@@ -102,7 +113,9 @@ def get_src(srcpkgname, srcpkgver):
         files = {
             "dsc": ["%s_%s.dsc" % (srcpkgname, srcpkgver)],
             "debian": ["%s_%s.debian.tar.xz" % (srcpkgname, srcpkgver)],
-            "orig": ["%s_%s.orig.tar.%s" % (srcpkgname, srcpkgver.split('-')[0], ext) for ext in ('gz', 'xz', 'bz2')]
+            "orig": [
+                "%s_%s.orig.tar.%s" % (srcpkgname, srcpkgver.split('-')[0], ext)
+                for ext in ('gz', 'xz', 'bz2')]
         }
         info = {
             "dsc": {},
@@ -120,9 +133,11 @@ def get_src(srcpkgname, srcpkgver):
                     info[key] = res
                     break
 
-        if info["dsc"].get("hash", None) and info["debian"].get("hash", None) and info["orig"].get("hash", None):
+        if info["dsc"].get("hash", None) and \
+                info["debian"].get("hash", None) and \
+                info["orig"].get("hash", None):
             status_code = info["dsc"]["status_code"]
-            result = {
+            api_result = {
                 "package": srcpkgname,
                 "version": srcpkgver,
                 "_comment": "foo",
@@ -161,68 +176,78 @@ def get_src(srcpkgname, srcpkgver):
                     ],
                 },
             }
-            result = json.dumps(result, indent=4) + "\n"
+            api_result = json.dumps(api_result, indent=4) + "\n"
 
-
-    return Response(result, status=status_code,
+    return Response(api_result, status=status_code,
                     mimetype="application/json")
 
 
 @app.route("/mr/binary/<string:pkg_name>/<string:pkg_ver>/binfiles",
            methods=["GET"])
+@cache.cached(timeout=3600)
 def get_bin(pkg_name, pkg_ver):
-    """
-    GET
-    """
-    result = {}
+    api_result = {}
     status_code = 404
+    debian_endpoint = \
+        '{base_url}/mr/binary/{pkg_name}/{pkg_ver}/binfiles?fileinfo=1'.format(
+            base_url=DEBIAN_SNAPSHOT, pkg_name=pkg_name, pkg_ver=pkg_ver)
+    try:
+        resp = requests.get(debian_endpoint)
+    except requests.exceptions.ConnectionError:
+        return Response(api_result, status=status_code,
+                        mimetype="application/json")
 
-    debian_endpoint = '{base_url}/mr/binary/{pkg_name}/{pkg_ver}/binfiles?fileinfo=1'.format(
-        base_url=DEBIAN_SNAPSHOT, pkg_name=pkg_name, pkg_ver=pkg_ver)
-    resp = requests.get(debian_endpoint)
     if resp.ok:
-        result = resp.content
+        api_result = resp.content
         status_code = resp.status_code
     else:
-        if pkg_name.startswith("lib"):
-            prefix = pkg_name[0:4]
-        else:
-            prefix = pkg_name[0]
-        deb = "%s_%s_amd64.deb" % (pkg_name, pkg_ver)
-        path = "pool/main/{prefix}/{pkg_name}".format(
-            prefix=prefix, pkg_name=pkg_name)
-        url = "https://deb.qubes-os.org/r4.1/vm/{path}/{deb}".format(
-            path=path, deb=deb)
-        info = get_file_info(url)
-        print(url)
-        if info.get("hash", None):
+        base_url = "https://deb.qubes-os.org/"
+        # to be changed to remote content
+
+        data = {}
+        info = {}
+        for arch in ("amd64", "all"):
+            deb = "%s_%s_%s.deb" % (pkg_name, pkg_ver, arch)
+            for f in get_repo_files():
+                if os.path.basename(f) == deb:
+                    url = base_url + f
+                    info = get_file_info(url)
+                    if info.get("hash", None):
+                        data[arch] = {}
+                        data[arch]["info"] = info
+                        data[arch]["file"] = deb
+                        data[arch]["url"] = url
+                        break
+
+        if data:
             status_code = info["status_code"]
-            result = {
+            result = [{"hash": data[arch]["info"]["hash"], "architecture": arch}
+                      for arch in data.keys()]
+            fileinfo = {}
+            for arch in data.keys():
+                fileinfo.update({
+                    data[arch]["info"]["hash"]: [
+                        {
+                            "name": data[arch]["file"],
+                            "archive_name": "debian",
+                            "path": "%s" % data[arch]["url"].replace(
+                                base_url, ''),
+                            "first_seen": data[arch]["info"]["first_seen"],
+                            "size": data[arch]["info"]["size"],
+                        }
+                    ],
+                }
+                )
+            api_result = {
                 "binary_version": pkg_ver,
                 "binary": pkg_name,
                 "_comment": "foo",
-                "result": [
-                    {
-                        "hash": info["hash"],
-                        "architecture": "amd64",
-                    }
-                ],
-                "fileinfo": {
-                    info["hash"]: [
-                        {
-                            "name": deb,
-                            "archive_name": "debian",
-                            "path": "/%s" % path,
-                            "first_seen": info["first_seen"],
-                            "size": info["size"],
-                        }
-                    ],
-                },
+                "result": result,
+                "fileinfo": fileinfo
             }
-            result = json.dumps(result, indent=4) + "\n"
+            api_result = json.dumps(api_result, indent=4) + "\n"
 
-
-    return Response(result, status=status_code,
+    return Response(api_result, status=status_code,
                     mimetype="application/json")
 
 
